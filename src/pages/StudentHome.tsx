c4a4abfbed1a3d8afbf0de9345383873b65e5f2d
Tin <ductin301@gmail.com>
2024-12-01 17:20:55 +0700

    fix studenthomepage

--- src/pages/StudentHome.tsx
+++ src/pages/StudentHome.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Container } from '@mui/material';
 
 import styles from './StudentHome.module.css';
 
 const StudentHome: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.welcomeText}>Welcome to the Home Page</h1>
-    </div>
-
+    <>
+      <Header />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <h1 className={styles.welcomeText}>Welcome to the Home Page</h1>
+      </Container>
+      <Footer />
+    </>
   );
 };
 
